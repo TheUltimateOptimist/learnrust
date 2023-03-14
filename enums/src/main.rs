dfdb73005823ce4ed52559e5bfa7a5f7105bf8d1
TheUltimateOptimist <digitalConfidence@web.de>
2023-03-14 01:36:58 +0100

    iflet

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -78,3 +78,29 @@ fn value_in_cents(coin: Coin) -> u8{
     }
 }
 
+fn ifletMain() {
+    let config_max = Some(3u8);
+    match config_max {
+        Some(max) => println!("The maximum is configured to be {}", max),
+        _ => (),
+    }
+    if let Some(max) = config_max {
+        println!("The maximum is configured to be {}", max);
+    }
+
+    let mut count = 0;
+    let coin = Coin::Nickel;
+    match coin {
+        Coin::Quarter(state) => println!("State quarter from {:?}!", state),
+        _ => count += 1,
+    }
+
+    let coin = Coin::Dime;
+    if let Coin::Quarter(state) = coin {
+        println!("State quarter from {:?}!", state);
+    }
+    else {
+        count += 1;
+    }
+}
+
